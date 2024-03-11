8a6804af87d0ce2d595e2e89ac5f088b243e9376
SamueleMaroli <133371511+SamueleMaroli@users.noreply.github.com>
2024-03-11 17:19:44 +0100

    aa

--- thinkOfANumber.groovy
+++ thinkOfANumber.groovy
@@ -1,7 +1,15 @@
 // test che non passa 
+import java.security.SecureRandom
+
+
+
 while (true) {
     try {
-        int x = Math.random() * 6
+        int x = new Math.random() *6
+        //for x in y 
+
+        int  a=0; 
+        
         print 'The computer has chosen a number between 0 and 5. Can you guess it?'
         def line = System.console().readLine()
         int g = line.toInteger()
@@ -11,9 +19,8 @@ while (true) {
         } else {
             println "Tough luck - you're wrong"
         }*/
-        
-        if(g == x){println "Well done" } else {println "Tough luck - you're wrong"}
 
+        if(g == x){println "Well done" } else {println "Tough luck - you're wrong" }
      } catch (NumberFormatException e) {
         println "The computer didn't understand '$line'"
     }
