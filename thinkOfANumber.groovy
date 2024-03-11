9f0bbf57e5906cb442d5976eda093b5b1ab9ecd8
SamueleMaroli <133371511+SamueleMaroli@users.noreply.github.com>
2024-03-11 15:17:12 +0100

    prova giusta

--- thinkOfANumber.groovy
+++ thinkOfANumber.groovy
@@ -19,18 +19,21 @@
 /** 
  * Simple game
  * @author: Jeremy Rayner
- * based on algorithms from INPUT/Marshall Cavendish/1984 prova git 2 es groovy test 
+ * based on algorithms from INPUT/Marshall Cavendish/1984 test buono
  */
 while (true) {
     try {
         int x = Math.random() * 6
-        print "The computer has chosen a number between 0 and 5. Can you guess it?"
-              
+        print 'The computer has chosen a number between 0 and 5. Can you guess it?'
         def line = System.console().readLine()
         int g = line.toInteger()
-        if(g == x){println "Well done"} else {println "Tough luck - you're wrong"}
-
+        if (g == x) {
+            println 'Well done'
+        } else {
+            println "Tough luck - you're wrong"
+        }
+     //        if(g == x){println "Well done"} else {println "Tough luck - you're wrong"}
      } catch (NumberFormatException e) {
-         println "The computer didn't understand '$line'"
-     }
+        println "The computer didn't understand '$line'"
+    }
 }
